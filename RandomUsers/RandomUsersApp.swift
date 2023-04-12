79deb0ee2b1483d3ad4a2273977e327cd1e7848d
Divino Borges <divino.borges@mindsdigital.net>
2023-04-12 17:52:42 -0300

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Divino Borges on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
